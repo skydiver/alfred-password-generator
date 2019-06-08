0f42fb3ab763c7dbad90329c97f34bd4638a9e3f
Martín M <skydiver@users.noreply.github.com>
2019-06-08 02:13:36 -0300

    script enhancements

--- main.js
+++ main.js
@@ -4,10 +4,11 @@ const { generate } = require('./lib/generator');
 const args = arg({
   '--type': String,
   '--length': Number,
+  '--params': String,
 });
 
-const type = args['--type'] || 'strong';
-const length = args['--length'] || 32;
+const type = args._[0] || args['--type'] || 'strong';
+const length = args._[1] || args['--length'] || 32;
 
 const password = generate(type, length);
 
