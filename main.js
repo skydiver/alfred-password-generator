62e68db488a080d9d8098f328ade447b607e39b1
Martín M <skydiver@users.noreply.github.com>
2019-06-08 01:58:57 -0300

    script refactor

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 const arg = require('arg');
-const { strong, alphanumeric, alpha } = require('./lib/generate-password');
-const { numeric } = require('./lib/strict-password');
+const { generate } = require('./lib/generator');
 
 const args = arg({
   '--type': String,
@@ -10,16 +9,6 @@ const args = arg({
 const type = args['--type'] || 'strong';
 const length = args['--length'] || 32;
 
-if (type === 'numeric') {
-  const password = numeric(length);
-  console.log(password);
-} else if (type === 'alpha') {
-  const password = alpha(length);
-  console.log(password);
-} else if (type === 'alphanum') {
-  const password = alphanumeric(length);
-  console.log(password);
-} else {
-  const password = strong(length);
-  console.log(password);
-}
+const password = generate(type, length);
+
+console.log(password);
