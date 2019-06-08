7bf72d3ff011adb0b4038400b1088f26abf7b68d
Martín M <skydiver@users.noreply.github.com>
2019-06-08 01:48:54 -0300

    functional password generator

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const arg = require('arg');
+const { strong, alphanumeric, alpha } = require('./lib/generate-password');
 const { numeric } = require('./lib/strict-password');
 
 const args = arg({
@@ -6,8 +7,19 @@ const args = arg({
   '--length': Number,
 });
 
+const type = args['--type'] || 'strong';
 const length = args['--length'] || 32;
 
-const password = numeric(length);
-
-console.log(password);
+if (type === 'numeric') {
+  const password = numeric(length);
+  console.log(password);
+} else if (type === 'alpha') {
+  const password = alpha(length);
+  console.log(password);
+} else if (type === 'alphanum') {
+  const password = alphanumeric(length);
+  console.log(password);
+} else {
+  const password = strong(length);
+  console.log(password);
+}
